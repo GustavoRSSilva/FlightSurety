f5bc76fdb56cf7b8a3ef3f4e40bd4b50aa5508ea
Adrian Mohnacs <amohnacss@gmail.com>
2019-05-11 15:19:36 -0700

    init register airline with mult-party consensus

--- contracts/FlightSuretyData.sol
+++ contracts/FlightSuretyData.sol
@@ -3,7 +3,12 @@ pragma solidity >= 0.4.25;
 import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract FlightSuretyData {
+
     using SafeMath for uint256;
+
+    struct Airline {
+        bool isRegistered;
+    }
 
     /********************************************************************************************/
     /*                                       DATA VARIABLES                                     */
@@ -11,6 +16,13 @@ contract FlightSuretyData {
 
     address private contractOwner;                                      // Account used to deploy contract
     bool private operational = true;                                    // Blocks all state changes throughout the contract if false
+
+    uint airlineCount = 0;
+    //Multi-party consensus values
+    uint votesNeeded = 0;
+    address[] approvalVotes;
+
+    mapping(address => Airline) airlines;
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -23,10 +35,11 @@ contract FlightSuretyData {
     */
     constructor
                                 (
-                                ) 
-                                public 
+                                )
+                                public
     {
         contractOwner = msg.sender;
+        this.registerAirline(msg.sender, true);
     }
 
     /********************************************************************************************/
@@ -38,10 +51,10 @@ contract FlightSuretyData {
 
     /**
     * @dev Modifier that requires the "operational" boolean variable to be "true"
-    *      This is used on all state changing functions to pause the contract in 
+    *      This is used on all state changing functions to pause the contract in
     *      the event there is an issue that needs to be fixed
     */
-    modifier requireIsOperational() 
+    modifier requireIsOperational()
     {
         require(operational, "Contract is currently not operational");
         _;  // All modifiers require an "_" which indicates where the function body will be added
@@ -64,11 +77,11 @@ contract FlightSuretyData {
     * @dev Get operating status of contract
     *
     * @return A bool that is the current operating status
-    */      
-    function isOperational() 
-                            public 
-                            view 
-                            returns(bool) 
+    */
+    function isOperational()
+                            public
+                            view
+                            returns(bool)
     {
         return operational;
     }
@@ -78,15 +91,32 @@ contract FlightSuretyData {
     * @dev Sets contract operations on/off
     *
     * When operational mode is disabled, all write transactions except for this one will fail
-    */    
+    */
     function setOperatingStatus
                             (
                                 bool mode
-                            ) 
+                            )
                             external
-                            requireContractOwner 
+                            requireContractOwner
     {
         operational = mode;
+    }
+
+    /**
+    * @dev Check if a airline is registered
+    *
+    * @return A bool that indicates if the user is registered
+    */
+    function isAirlineRegistered
+                            (
+                                address airlineAddress
+                            )
+                            private
+                            view
+                            returns(bool)
+    {
+        require(airlineAddress != address(0), "'airlineAddress' must be a valid address.");
+        return airlines[airlineAddress].isRegistered;
     }
 
     /********************************************************************************************/
@@ -97,22 +127,70 @@ contract FlightSuretyData {
     * @dev Add an airline to the registration queue
     *      Can only be called from FlightSuretyApp contract
     *
-    */   
+    *   Enter a new airline up to four.  After four, 50% consesnus from airlines is required for entry
+    *
+    */
     function registerAirline
-                            (   
+                            (
+                                address airline
                             )
                             external
                             pure
+                            requireContractOwner
     {
+        require(!isAirlineRegistered(airline), "Airline is already registered");
+
+        if (votesNeeded <= 4) { //only existing airlines may register a new airline until four are registered
+            require(airlines[msg.sender].isRegistered, "Airlines must be registered prior to 4 registered airlines");
+        
+            airlines[airline] = Airline({
+                                    isRegistered: true
+                                });
+            airlineCount ++;
+            votesNeeded = SafeMath.div(airlineCount, 2);
+        } else {
+            //After four 50% consensys is required from registered airlines
+
+            //until we reach the amount needed for consensys we just add the address to our list of approval voters
+            //multiple address are required to call this contract
+            if(approvalVotes.length < votesNeeded) {
+
+                bool isDuplicate = false;
+            
+                for(uint i = 0; i < approvalVotes.length; i++) {
+                    //check for duplicates to add to list of approvers
+                    if(approvalVotes[i] == msg.sender) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                require(!isDuplicate, "Caller has already called this function");
+
+                approvalVotes.push(msg.sender);
+
+            } else { //we have enough votes let's add it to the airlines mapping
+
+                //we need to clear approval votes
+            }
+
+            //we may need to move this, set only if 50% consensys
+            votesNeeded = SafeMath.div(votesNeeded, 2);
+        }
+    }
+
+    function addAirlineToMapping(
+
+                            ) private {
+
     }
 
 
    /**
     * @dev Buy insurance for a flight
     *
-    */   
+    */
     function buy
-                            (                             
+                            (
                             )
                             external
                             payable
